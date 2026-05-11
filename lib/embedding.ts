9586ec3241b72ebd3f858a0aa7d519711f95bd5d
Zach Estrella <zachgabriel.estrella@ziaplex.biz>
2026-05-12 05:14:29 +0800

    Remove recent change

--- lib/embedding.ts
+++ lib/embedding.ts
@@ -2,23 +2,17 @@
 
 import { pipeline, env } from "@xenova/transformers";
 
-// FORCE WASM ONLY
-env.backends.onnx = false;
+// VERY IMPORTANT
 env.allowLocalModels = false;
 
-let extractor:any = null;
-
 export async function getEmbedding(text:string) {
-  if (!extractor) {
-    extractor = await pipeline(
-      "feature-extraction",
-      "Xenova/all-MiniLM-L6-v2",
-      {
-        quantized: true,
-      }
-    );
-  }
-
+  const extractor = await pipeline(
+    "feature-extraction",
+    "Xenova/all-MiniLM-L6-v2",
+    {
+      quantized: true,
+    }
+  );
   const output = await extractor(text, {
     pooling: "mean",
     normalize: true,
