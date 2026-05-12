a1dc864f6ccd05a005fbf850eff4f9a488ffa514
Zach Estrella <zachgabriel27@gmail.com>
2026-05-12 17:52:56 +0800

    Test run

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
-import { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  serverExternalPackages: ['@xenova/transformers'],
-};
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["sharp", "onnxruntime-node","@xenova/transformers"],
+  },
+  webpack: (config:any) => {
+    // Ignore node-specific modules when bundling for the browser
+    // https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
+}
